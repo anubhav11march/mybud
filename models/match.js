3949ff0c5e0f9731a3176cf849a4b965b8db9575
vikalp2502 <vikalpkaushik97@gmail.com>
2022-06-17 13:55:27 +0530

    fix ugs

--- models/match.js
+++ models/match.js
@@ -1,23 +1,20 @@
-const mongoose=require('mongoose');
-const {Schema}=mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const MatchSchema= new Schema({
-    users:[
-        {type:mongoose.Types.ObjectId}
-    ],
-    time:{
-        type:Date,
-        default:Date.now
-    },
-    finaltime:{
-        type:Date
-    }
-},{
-    timestamps:{type:Date,default:Date.now}
-});
+const MatchSchema = new Schema(
+	{
+		users: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+		time: {
+			type: Date,
+			default: Date.now,
+		},
+		finaltime: {
+			type: Date,
+		},
+	},
+	{
+		timestamps: { type: Date, default: Date.now },
+	}
+);
 
-
-
-module.exports=  mongoose.model("matches",MatchSchema);
-
-
+module.exports = mongoose.model('matches', MatchSchema);
